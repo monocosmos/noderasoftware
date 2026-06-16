f668f60ec557ed51adcc2cccd2d478c8106d94dd
Hasan Fırat Keskin <keskinfirathasan@gmail.com>
2026-06-16 21:16:35 +0300

    Publish Android 1.0.21 APK

--- apps/android/app/build.gradle.kts
+++ apps/android/app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         minSdk = 23
         targetSdk = 36
         // versionName kullaniciya gorunen surumdur; versionCode teknik guncelleme sirasidir.
-        versionCode = 2026061605
-        versionName = "1.0.20"
+        versionCode = 2026061606
+        versionName = "1.0.21"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
